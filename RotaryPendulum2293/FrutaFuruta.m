e7bd66c6a6a8b6437689790fce23246c2fae4955
amartorell <amartorell@itba.edu.ar>
2021-11-11 17:10:45 -0300

    arranque a poner las matrices

--- RotaryPendulum2293/FrutaFuruta.m
+++ RotaryPendulum2293/FrutaFuruta.m
@@ -7,6 +7,11 @@ aw1= 5.71e+4; %sacado del grafico con link 1 bien armado
 j1= 1/(aw1);   %obtenemos el momento de inercia con respecto al punto de pivote
 L1=0.1035;
 
+%cosntantes generales
+g=9.81;
+b1=1e-6;
+b2=1e-6;
+
 % datos del sistema, joint 2
 m2a= 0.00013195457400179582 + 0.00052672018519800228 + 0.00043271308549453917 + 0.00086673289514495463;
 m2b= 0.00043271308549453917 + 0.0010695037303870972 + 0.0012730475598330083 + 0.00043271308549453917 + 0.00058767552757679558;
@@ -28,4 +33,25 @@ j2=(T/(2*pi))^2*m2*g*l2;
 J1= j1;
 J2 = j2 + m2*l2^2;
 J0= J1 + m2*L1;
+
+m2l2 = m2*l2;       %producto utilizado varias veces
+
+denom = (J0*J2-(m2l2*L1)^2);
+
+a32= (g*m2l2^2*L1)/denom;
+a33= (-b1*J2)/denom;
+a34= (-b2*m2l2*L1)/denom;
+
+a42= (g*m2l2*J0)/denom;
+a43= (-b1*m2l2*L1)/denom;
+a44=(-b2*J0)/denom;
+
+b31=J2/denom;
+
+
+
+
+
+
+
 
