7cb50913c923a6ade9e749340ba204674c2d52b4
amartorell <amartorell@itba.edu.ar>
2021-11-14 18:54:47 -0300

    control lqr continuo

--- RotaryPendulum2293/FrutaFuruta.m
+++ RotaryPendulum2293/FrutaFuruta.m
@@ -1,32 +1,23 @@
 % datos del sistema, joint 1
-m1a = 0.0011644908507951884 + 0.0011644908507951884 + 0.0011644908507951884 + 0.00058767552757679601 + 0.0010321252629775849 + 0.012717581311371812;
-m1b = 0.00052672018519800228;
-m1 = m1a + m1b ; 
+m1 =0.01835; %0.0198; 
+g=9.81;
+T=725.926e-3;
 
-aw1= 5.71e+4; %sacado del grafico con link 1 bien armado
+
+aw1= 5.71e4; %sacado del grafico con link 1 bien armado
 j1= 1/(aw1);   %obtenemos el momento de inercia con respecto al punto de pivote
 L1=0.1035;
-
+l1 = j1/(m1*g*((T/(2*pi))^2));
 %cosntantes generales
-g=9.81;
 b1=1e-6;
-b2=1e-6;
+b2=1*1e-6;
 
 % datos del sistema, joint 2
-m2a= 0.00013195457400179582 + 0.00052672018519800228 + 0.00043271308549453917 + 0.00086673289514495463;
-m2b= 0.00043271308549453917 + 0.0010695037303870972 + 0.0012730475598330083 + 0.00043271308549453917 + 0.00058767552757679558;
-m2= m2a+m2b;
-
+m2 = 0.00575; %3.19e-3;
 % experimento d, sacamos la distancia al centro de masa del joint 2
- l2=(2e-3)/(m2*9.81*sin(0.6592));
- 
- 
- 
+l2=(2e-3)/(m2*9.81*sin(0.6592)); 
 % experimento c, sacamos el periodo del pendulo obtenemos inercia de 2
-T=725.926e-3;
-j2=(T/(2*pi))^2*m2*g*l2;
-
-
+j2=((T/(2*pi))^2)*m2*g*l2;
 
 %linearizacion del modelo
 
@@ -64,22 +55,22 @@ B=[0 0;
    b31 b32;
    b41 b42];
 
-B1=[0; 0 ; b31 ; b41];
+B1=[0; 0 ; b31 ; b41]/1000;
    
 C= [ 1 0 0 0];
+
 
 rank(ctrb(A,B)); %cheque controlabilidad del sistema
 rank(obsv(A,C)); %chequeo observavilidad del sistema
 
 sys=ss(A,B1,C,0); 
-Q=diag([3 3 3 3 1]);
-R=2;
+Q=diag([50 2 20 3 0.1]);
+Q2=diag([1 10 1 10]);
+R=1;
 [K,S,E]=lqi(sys,Q,R);
-
+[K1,S1,E2]=lqr(sys,Q2,R);
 Kp=K(1:4);
 ki=K(5);
 
 Aclp = (A-B1*Kp);
 nsys=ss(Aclp,B1,C,0);
-t=0:0.1:100;
-step(nsys,t)
